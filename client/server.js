2dcc68bb75fc5f09de7cd8c7093954ff62ec3830
Lonis <164430029+lonis752@users.noreply.github.com>
2025-02-24 16:38:50 -0800

    fixed dist directions for deployment

--- client/server.js
+++ client/server.js
@@ -38,10 +38,10 @@ app.get("/code", async (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/dist")));
+  app.use(express.static(path.join(__dirname, "/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
   });
 }
 
